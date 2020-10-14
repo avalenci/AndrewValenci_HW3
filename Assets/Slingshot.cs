9320d448ddb249568bd194cf506f1e3166176bad
avalenci <ansmeew@gmail.com>
2020-10-14 17:52:34 -0400

    scripted camera to follow projectile

--- Assets/Slingshot.cs
+++ Assets/Slingshot.cs
@@ -74,6 +74,7 @@ public class Slingshot : MonoBehaviour
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
         }
     }
