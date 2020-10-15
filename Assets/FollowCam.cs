eddd5c43bbadba8a1a11b281d56d5bdc3e7abb19
avalenci <ansmeew@gmail.com>
2020-10-15 19:29:04 -0400

    updated camera movement

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -8,6 +8,7 @@ public class FollowCam : MonoBehaviour
 
     [Header("Set in Inspector")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Set Dynamically")]
     public float camZ;
@@ -22,8 +23,12 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
 
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
+
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
