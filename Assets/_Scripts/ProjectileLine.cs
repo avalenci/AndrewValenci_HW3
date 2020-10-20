14876656829d4073fc5763183e63d25a8ea26932
avalenci <ansmeew@gmail.com>
2020-10-20 17:54:57 -0400

    implemented multiple projectile lines

--- Assets/_Scripts/ProjectileLine.cs
+++ Assets/_Scripts/ProjectileLine.cs
@@ -8,16 +8,30 @@ public class ProjectileLine : MonoBehaviour
 
     [Header("Set in Inspector")]
     public float            minDist = 0.1f;
+    public GameObject[] lines = new GameObject[4];
 
-    private LineRenderer    line;
     private GameObject      _poi;
     private List<Vector3>   points;
+    private int lineNumb;
+    private bool firstShot;
 
     private void Awake()
     {
         S = this;
-        line = GetComponent<LineRenderer>();
-        line.enabled = false;
+
+        firstShot = true;
+
+        for (int i = 0; i < 4; i++)
+        {
+            lines[i] = Instantiate<GameObject>(lines[i]);
+            Material mat = lines[i].GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = (float)(1.0 - (0.25 * i));
+            mat.color = c;
+            lines[i].GetComponent<Renderer>().material = mat;
+        }
+
+        lines[lineNumb].GetComponent<LineRenderer>().enabled = false;
         points = new List<Vector3>();
     }
 
@@ -32,9 +46,7 @@ public class ProjectileLine : MonoBehaviour
             _poi = value;
             if (_poi != null)
             {
-                line.enabled = false;
-                points = new List<Vector3>();
-                AddPoint();
+                NextShot();
             }
         }
     }
@@ -42,11 +54,33 @@ public class ProjectileLine : MonoBehaviour
     public void Clear()
     {
         _poi = null;
-        line.enabled = false;
+        foreach (GameObject line in lines)
+        {
+            line.GetComponent<LineRenderer>().enabled = false;
+        }
+        lineNumb = 0;
+        firstShot = true;
         points = new List<Vector3>();
     }
 
-    public void AddPoint()
+    public void NextShot()
+    {
+        if (lineNumb == 3)
+        {
+            Clear();
+        } else if (firstShot)
+        {
+            firstShot = false;
+        } else
+        {
+            lineNumb++;
+        }
+        points = new List<Vector3>();
+
+        AddPoint(lines[lineNumb].GetComponent<LineRenderer>());
+    }
+
+    public void AddPoint(LineRenderer line)
     {
         Vector3 pt = _poi.transform.position;
         if (points.Count > 0 && (pt - lastPoint).magnitude < minDist)
@@ -106,7 +140,7 @@ public class ProjectileLine : MonoBehaviour
             }
         }
 
-        AddPoint();
+        AddPoint(lines[lineNumb].GetComponent<LineRenderer>());
         if (FollowCam.POI == null)
         {
             poi = null;
